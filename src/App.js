b15086e9cc629fe1a104b9818527f83e9d2328a5
Prashanth Deekonda <deekondapk2@upmc.edu>
2022-02-16 20:50:15 -0500

    search box integration

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
-import {CardList} from './components/card-list/card-list.component';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <header className="App-header">
-          <CardList monsters={this.state.monsters} />
-        </header>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={((e) =>
+            this.setState({ searchField: e.target.value })
+          )}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
 }
-
-
 
 export default App;
