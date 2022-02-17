16d4b2603da6aa2deaf41b8824e0ce33f5613a50
Prashanth Deekonda <deekondapk2@upmc.edu>
2022-02-17 12:37:44 -0500

    bind event for lexical scoping

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './search-box.styles.css';
 
-export const SearchBox = ({placeholder, handleChange}) => {
+export const SearchBox = ({ placeholder, handleChange }) => {
     return (
         <input
             type="search"
