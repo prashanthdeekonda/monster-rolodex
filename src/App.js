ab07001ef5a8e3f10f1a5791e223439c0993c0c2
Prashanth Deekonda <deekondapk2@upmc.edu>
2022-02-16 21:00:06 -0500

    search box reusable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -19,16 +20,15 @@ class App extends Component {
   }
 
   render() {
-    const {monsters, searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monsters"
-          onChange={((e) =>
-            this.setState({ searchField: e.target.value })
-          )}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
